2de2a8a1b9ebfd7edd5afeb21aa560ec0ee80aed
sadikBr <brahimsadik98@gmail.com>
2020-10-13 17:35:29 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawer" app clipped> </v-navigation-drawer>
+
+      <v-app-bar app clipped-left color="red" dense>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-icon class="mx-4" large>
+          mdi-music
+        </v-icon>
+        <v-toolbar-title class="mr-12 align-center">
+          <span class="title">Music Player</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-row align="center" style="max-width: 650px">
+          <v-text-field
+            :append-icon-cb="() => {}"
+            placeholder="Search..."
+            single-line
+            append-icon="mdi-magnify"
+            color="white"
+            hide-details
+          ></v-text-field>
+        </v-row>
+      </v-app-bar>
+
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-app>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    drawer: false,
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*::-webkit-scrollbar {
+  display: none;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+/* Hide scrollbar for IE, Edge and Firefox */
+* {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 }
 </style>

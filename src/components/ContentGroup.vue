3ab132bb971a8bbf64e67435274254be8906515d
sadikBr <brahimsadik98@gmail.com>
2020-10-13 20:43:49 +0100

    added horizontal scroll

--- src/components/ContentGroup.vue
+++ src/components/ContentGroup.vue
@@ -2,17 +2,23 @@
   <v-item-group multiple>
     <v-container>
       <h1>Trending Songs</h1>
-      <v-row>
-        <v-col v-for="n in 5" :key="n" cols="12" md="4">
+      <v-row class="cards-container">
+        <v-col
+          class="d-inline-block dimensions"
+          v-for="n in 5"
+          :key="n"
+          cols="12"
+          mb="2"
+        >
           <v-item>
             <v-card class="mx-auto" light>
               <v-img
                 src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                height="200px"
+                height="150px"
               ></v-img>
 
               <v-row align="center">
-                <v-col cols="10">
+                <v-col class="padding" cols="9">
                   <v-card-title>
                     Top western road trips
                   </v-card-title>
@@ -21,8 +27,8 @@
                     1,000 miles of wonder
                   </v-card-subtitle>
                 </v-col>
-                <v-col cols="2">
-                  <v-icon class="pointer">mdi-pause</v-icon>
+                <v-col class="padding" cols="3">
+                  <v-icon class="pointer">mdi-play</v-icon>
                 </v-col>
               </v-row>
             </v-card>
@@ -37,7 +43,26 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+.cards-container {
+  display: inline-block !important;
+  white-space: nowrap !important;
+  overflow: auto hidden !important;
+}
+.dimensions {
+  width: 300px !important;
+}
+.padding {
+  padding: 0 12px !important;
+
+  div {
+    font-size: 1em !important;
+
+    &:last-child {
+      font-size: 0.8em !important;
+    }
+  }
+}
 .pointer {
   cursor: pointer;
 }
